f2066eb720f3bbfde359fca05415926b3a0b5b8f
burkeow <burkeowen98@gmail.com>
2025-02-23 16:26:55 +1100

    convert to std::print

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,20 +1,16 @@
 #include <arpa/inet.h>
+#include <cstdlib>
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <print>
 
 int main(int argc, char *argv[]) {
-  // Flush after every std::cout / std::cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
-  // You can use print statements as follows for debugging, they'll be visible
-  // when running tests.
-  std::cerr << "Logs from your program will appear here" << std::endl;
+  std::print(std::cerr, "Logs from your program will appear here");
 
   if (argc != 3) {
-    std::cerr << "Expected two arguments" << std::endl;
-    return 1;
+    std::print(std::cerr, "Expected two arguments");
+    return EXIT_FAILURE;
   }
 
   std::string database_file_path = argv[1];
@@ -23,15 +19,14 @@ int main(int argc, char *argv[]) {
   if (command == ".dbinfo") {
     std::ifstream database_file(database_file_path, std::ios::binary);
     if (!database_file) {
-      std::cerr << "Failed to open the database file" << std::endl;
-      return 1;
+      std::print(std::cerr, "Failed to open the database file");
+      return EXIT_FAILURE;
     }
     database_file.seekg(16); // Skip the first 16 bytes of the header
     char buffer[2];
     database_file.read(buffer, 2);
     auto const page_size = ntohs(std::bit_cast<uint16_t>(buffer));
-    std::cout << "database page size: " << page_size << std::endl;
+    std::print("database page size: {}", page_size);
   }
-
-  return 0;
+  return EXIT_SUCCESS;
 }
